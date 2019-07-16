3f945863ce322e8f3508a3f602264d5cf1740907
stone1803 <51925355+stone1803@users.noreply.github.com>
2019-07-16 10:18:52 +0700

    usercoinmakt

--- coinmaket.py
+++ coinmaket.py
@@ -1,6 +1,7 @@
 import requests
 import json
-
+import pyfiglet
+from prettytable import PrettyTable
 while True:
     # base URLs
     globalURL = "https://api.coinmarketcap.com/v1/global/"
@@ -10,26 +11,26 @@ while True:
     request = requests.get(globalURL)
     data = request.json()
     globalMarketCap = data['total_market_cap_usd']
-    print(data)
     # menu
-    print()
-    print("Welcome to the CoinMarketCap Explorer!")
-    print("Global cap of all cryptocurrencies: $" + str(globalMarketCap))
-    print("Enter 'all' or 'name of crypto' (i.e. bitcoin) to see the name of the top 100 currencies or a specific currency")
+    print(pyfiglet.figlet_format('CoinMarketCap'))
+    print("Kiem tra gia CoinMarketCap !")
+    print("Tong giao dich tren thi truong: $" + str(globalMarketCap))
+    print("Go all de xem 100 coin top list or co the go chinh xac ten 1 coin nhu bitcoin da duoc list tai coinmarketcap")
     print()
     choice = input("Your choice: ")
 
     if choice == "all":
         request = requests.get(tickerURL)
         data = request.json()
-
-        for x in data:
-            ticker = x['symbol']
-            price = x['price_usd']
-
-            print(ticker + ":\t\t$" + price)
-        print()
-
+        for y in data:
+            x = PrettyTable()
+            x.field_names = ["TEN COIN", "GIA USD"]
+            ticker = y['symbol']
+            price = y['price_usd']
+            x.add_row([ticker,price])
+            print(x)
+            # print(ticker + ":\t\t$" + price)
+            print()
     else:
         tickerURL += '/'+choice+'/'
         request = requests.get(tickerURL)
@@ -42,9 +43,9 @@ while True:
         print('biên độ 24 h qua ',percent_change_24h)
 
 
-    choice2 = input("Again? (y/n): ")
+    choice2 = input("Kiem tra lai ? (co/khong): ")
 
-    if choice2 == "y":
+    if choice2 == "co":
         continue
-    if choice2 == "n":
+    if choice2 == "khong":
         break
